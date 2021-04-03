16730ea9096dbdd29e80330beb3b5653fdf744e6
NeAlex9 <68583405+NeAlex9@users.noreply.github.com>
2021-04-03 13:14:58 +0300

    13.Brush is done

--- Figures/Brush.cs
+++ Figures/Brush.cs
@@ -9,17 +9,38 @@ namespace AlexPaint
 
         public override void PrepareForDrawing(MouseEventArgs e, DrawingAssets assets)
         {
+            if ((e.Button & MouseButtons.Left) != 0)
+            {
+                Points.Add(new Point(e.X, e.Y));
+                Points.Add(new Point(e.X + 1, e.Y));
+            }
+        }
 
+        public void DrawFigure(Graphics g, Pen myPen)
+        {
+            int len = Points.Count;
+            g.DrawLine(myPen, Points[len - 2], Points[len - 1]);
         }
 
         public override void DrawWhileMouseMove(MouseEventArgs e, DrawingAssets assets, PictureBox DrawPanel)
         {
-
+            if ((e.Button & MouseButtons.Left) != 0)
+            {
+                Points.Add(new Point(e.X, e.Y));
+                Graphics g = Graphics.FromImage(assets.MainCanvas);
+                DrawFigure(g, assets.MyPen);
+                DrawPanel.Image = assets.MainCanvas;
+                DrawPanel.Refresh();
+            }
         }
 
         public override void SetFigure(MouseEventArgs e, DrawingAssets assets, PictureBox DrawPanel)
         {
-
+            if ((e.Button & MouseButtons.Left) != 0)
+            {
+                DrawWhileMouseMove(e, assets, DrawPanel);
+                FinishPainting();
+            }
         }
 
         public override void FinishPainting()
